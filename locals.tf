287acf5234e4186b4d3ad72ca2687f2740b7382e
Peter Svensson <peter@sparetimecoders.com>
2021-10-14 15:55:15 +0200

    bug: honor the master_count variable

--- locals.tf
+++ locals.tf
@@ -57,7 +57,7 @@ locals {
   private_subnets_enabled  = length(var.private_subnet_ids) > 0
   node_group_subnet_prefix = local.private_subnets_enabled ? "private-${var.region}" : "utility-${var.region}"
   topology                 = local.private_subnets_enabled ? "private" : "public"
-  master_subnets_zones     = local.private_subnets_enabled ? keys(var.private_subnet_ids) : slice(keys(var.public_subnet_ids), 0, var.master_count)
+  master_subnets_zones     = local.private_subnets_enabled ? slice(keys(var.private_subnet_ids), 0, var.master_count) : slice(keys(var.public_subnet_ids), 0, var.master_count)
 
   secrets = (length(compact([var.docker_config])) > 0) ? [""] : []
 }
