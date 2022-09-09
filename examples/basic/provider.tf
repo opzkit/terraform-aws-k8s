54f8d61c800e307aefa9e1651734977c11d2d378
Peter Svensson <peter@sparetimecoders.com>
2022-09-09 11:23:12 +0200

    chore: use s3_use_path_style instead of deprecated s3_force_path_style

--- examples/basic/provider.tf
+++ examples/basic/provider.tf
@@ -6,7 +6,7 @@ provider "aws" {
   skip_requesting_account_id  = true
   skip_credentials_validation = true
   skip_metadata_api_check     = true
-  s3_force_path_style         = true
+  s3_use_path_style           = true
   region                      = "eu-west-1"
   access_key                  = "mock_access_key"
   secret_key                  = "mock_secret_key"
