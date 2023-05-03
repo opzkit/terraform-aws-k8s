266bd3047f470e2808d6e04b5f811f321daa968d
Peter Svensson <peter@sparetimecoders.com>
2023-05-03 08:22:05 +0200

    chore: change irsa bucket ACL policy

--- s3.tf
+++ s3.tf
@@ -3,17 +3,22 @@ resource "aws_s3_bucket" "issuer" {
   force_destroy = true
 }
 
-resource "aws_s3_bucket_acl" "issuer" {
+resource "aws_s3_bucket_ownership_controls" "issuer" {
   bucket = aws_s3_bucket.issuer.id
-  acl    = "public-read"
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "issuer" {
+  bucket = aws_s3_bucket.issuer.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 moved {
   from = aws_s3_bucket.issuer[0]
   to   = aws_s3_bucket.issuer
-}
-
-moved {
-  from = aws_s3_bucket_acl.issuer[0]
-  to   = aws_s3_bucket_acl.issuer
 }
