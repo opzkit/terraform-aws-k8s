bc67648e6359b98413c1b64d907e9252fc907e64
Peter Svensson <peter@sparetimecoders.com>
2026-03-16 09:15:50 +0100

    fix(commitlint): increase header max length to 115 characters (#298)
    Update the maximum header length in the commitlint configuration 
    to 115 characters to accommodate longer commit messages while 
    maintaining readability. This change enhances flexibility for 
    developers when crafting detailed commit messages.

--- .github/commitlint.config.mjs
+++ .github/commitlint.config.mjs
@@ -4,7 +4,7 @@ export default {
    * Any rules defined here will override rules from @commitlint/config-conventional
    */
   rules: {
-    'header-max-length': [2, 'always', 105],
+    'header-max-length': [2, 'always', 115],
     'body-max-line-length': [2, 'always', 200],
   },
 };
